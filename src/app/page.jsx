bfd0be5231c2ffd80ab3822e803a38c7fab3f119
jrohrs <jrohrs@wisc.edu>
2023-11-11 16:23:06 -0600

    initial project

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Nav from "./components/Nav";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Nav />
+      <div className="flex min-h-screen justify-center ">
+        <h1 className=" text-center lg:text-9xl md:text-7xl text-5xl m-40">
+          MadFunds
+        </h1>
+      </div>
+    </main>
+  );
+}
