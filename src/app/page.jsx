5c8da18db49b8f4820794fd665079f979ffdb9c8
jrohrs <jrohrs@wisc.edu>
2023-11-11 20:54:01 -0600

    fundraiser form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
-import Nav from "../components/Nav";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
     <main className="">
-      <Nav />
       <div className="flex min-h-screen justify-center ">
         <h1 className=" text-center lg:text-9xl md:text-7xl text-5xl m-40">
           MadFunds
